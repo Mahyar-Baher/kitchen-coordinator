89588023d87bde69ef3c381fe3cddbd7b5450a7a
KawikaKN <kknjr16@gmail.com>
2026-01-27 16:46:40 -1000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // This tells Vercel to include the Prisma WASM files in the serverless function
-  outputFileTracingIncludes: {
-    '/**': [
-      './node_modules/@prisma/client/runtime/*.js',
-      './node_modules/@prisma/client/runtime/*.wasm',
-      './node_modules/.prisma/client/*.so.node'
-    ],
+  experimental: {
+    outputFileTracingIncludes: {
+      '/**': [
+        './node_modules/@prisma/client/runtime/*.js',
+        './node_modules/@prisma/client/runtime/*.wasm',
+        './node_modules/.prisma/client/*.so.node'
+      ],
+    },
   },
   images: {
     remotePatterns: [
